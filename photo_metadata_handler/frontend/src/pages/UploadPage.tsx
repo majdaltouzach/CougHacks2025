e92e6cf18a8a1b7b5c209fe60d6fd8a03be38b6f
Fleronin <LilMoses124@hotmail.com>
2025-04-04 19:22:57 -0700

    added to uploadForm and UploadPage

---
+++ photo_metadata_handler/frontend/src/pages/UploadPage.tsx
@@ -0,0 +1,9 @@
+//Denny's part of the project
+import './App.css';
+import Layout from '../components/Layout';
+import UploadForm from '../components/UploadForm';
+
+function UploadPage(){
+    return(1);
+}
+export default UploadPage;
