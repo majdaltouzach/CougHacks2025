7c6c9f3bd271e799055fab362a40b9115924c7c4
Yakup <yakup.atahanow.b@gmail.com>
2025-04-05 08:59:14 -0700

    Organized the file locations

--- photo_metadata_handler/frontend/src/components/UploadForm.tsx
+++ photo_metadata_handler/frontend/src/components/UploadForm.tsx
@@ -2,41 +2,36 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import '../UploadForm.css'
+import "./UploadForm.css";
 
 const UploadForm = () => {
-    const [file, setFile] = useState<File | null>(null);
-    const navigate = useNavigate();
-    const askInput = "Upload an Image";
-    const handleUpload = async () => {
-      if (!file) return;
-  
-      const formData = new FormData();
-      formData.append("file", file);
-  
-      const res = await axios.post("http://localhost:8000/upload/", formData);
-      localStorage.setItem("metadata", JSON.stringify(res.data.metadata));
-      localStorage.setItem("filename", res.data.filename);
-      navigate("/metadata");
-    };
-  
-    return (
-      <div className = "uploadContainer">
-        
-        <img
-          src="/stockImgIcon.png"
-          alt="logo192.png"
-          className="uploadImage"
-        />
-        <h1 className="header"> { askInput } </h1>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => setFile(e.target.files?.[0] || null)}
-        />
-        <button onClick={handleUpload}>Upload</button>
-      </div>
-    );
+  const [file, setFile] = useState<File | null>(null);
+  const navigate = useNavigate();
+  const askInput = "Upload an Image";
+  const handleUpload = async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await axios.post("http://localhost:8000/upload/", formData);
+    localStorage.setItem("metadata", JSON.stringify(res.data.metadata));
+    localStorage.setItem("filename", res.data.filename);
+    navigate("/metadata");
   };
-  
-  export default UploadForm;
+
+  return (
+    <div className="uploadContainer">
+      <img src="/stockImgIcon.png" alt="logo192.png" className="uploadImage" />
+      <h1 className="header"> {askInput} </h1>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+      />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default UploadForm;
