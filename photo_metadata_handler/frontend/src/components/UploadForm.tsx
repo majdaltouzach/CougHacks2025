3f23cb5e6891c5a1316b05a586c864e969078aa1
Yakup <yakup.atahanow.b@gmail.com>
2025-04-05 09:58:12 -0700

    Updated the shared button class for all htmls to use

--- photo_metadata_handler/frontend/src/components/UploadForm.tsx
+++ photo_metadata_handler/frontend/src/components/UploadForm.tsx
@@ -26,7 +26,7 @@ const UploadForm = () => {
         accept="image/*"
         onChange={(e) => setFile(e.target.files?.[0] || null)}
       />
-      <button onClick={handleUpload} className="button first-button">
+      <button onClick={handleUpload} className="button no-margin-button">
         Upload
       </button>
     </div>
