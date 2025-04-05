8dcf918afd319bc5b4bd5c914c2383aa1e382d79
Yakup <yakup.atahanow.b@gmail.com>
2025-04-04 21:52:44 -0700

    Update before merge

--- photo_metadata_handler/frontend/src/pages/UploadPage.tsx
+++ photo_metadata_handler/frontend/src/pages/UploadPage.tsx
@@ -1,9 +1,34 @@
-//Denny's part of the project
-import './App.css';
-import Layout from '../components/Layout';
-import UploadForm from '../components/UploadForm';
+// UploadForm.tsx
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function UploadPage(){
-    return(1);
-}
-export default UploadPage;
+const UploadForm = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const navigate = useNavigate();
+
+  const handleUpload = async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await axios.post("http://localhost:8000/upload/", formData);
+    localStorage.setItem("metadata", JSON.stringify(res.data.metadata));
+    localStorage.setItem("filename", res.data.filename);
+    navigate("/metadata");
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+      />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default UploadForm;
